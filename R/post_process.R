3e53e0736275e6bc0b9b788be826f9810800bc51
Lorenzo Cattarino <lcattari@dide.local>
2019-11-12 12:30:14 +0000

    remove function

--- R/post_process.R
+++ R/post_process.R
@@ -7,11 +7,3 @@ post_process <- function(i, dat, time, exp_des) {
   cbind(id = i, time = time, x)  
   
 }
-
-# mean_of_the_patches <- function(x){
-#   
-#   # browser()
-#   
-#   apply(x[,1:20], 1, mean)
-#   
-# }

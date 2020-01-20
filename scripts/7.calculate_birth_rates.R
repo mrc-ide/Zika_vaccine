0983f5f61860be19a73e0d49ca02784b03a078f5
Lorenzo Cattarino <lcattari@dide.local>
2020-01-20 14:53:23 +0000

    calculate birth rates per time step and plot them

--- scripts/7.calculate_birth_rates.R
+++ scripts/7.calculate_birth_rates.R
@@ -1,7 +1,23 @@
 
 # calculate age specific birth rates
 
+library(ggplot2)
+
 source(file.path("R", "utility_functions.R"))
+
+
+# define parameters -----------------------------------------------------------
+
+
+na <- 11
+
+YL <- 364
+
+DT <- 1
+
+
+# -----------------------------------------------------------------------------
+
 
 # age specific fertility rates per 5yr windows (live births per 1000 women) 
 br_brazil_all_years <- read.csv(file.path("data", "age_specific_birth_rates_Brazil.csv"))
@@ -24,8 +40,6 @@ for (i in seq_along(my_seq)) {
   
 }
 
-na <- 11
-
 br_brazil_age <- rep(0, na)
 
 br_brazil_age[1] <- 0
@@ -40,4 +54,20 @@ br_brazil_age[9] <- 0
 br_brazil_age[10] <- 0
 br_brazil_age[11] <- 0
 
-write_out_rds(br_brazil_age, "output", "age_specific_birth_rates")
+br_brazil_age_2 <- br_brazil_age / YL * DT
+  
+df_to_plot <- data.frame(age = seq_len(na), 
+                         br = br_brazil_age_2)
+
+p <- ggplot() +
+  geom_bar(data = df_to_plot, aes(age, br), stat = "identity") +
+  scale_x_continuous("age groups", 
+                     breaks = seq_len(na), 
+                     labels = c("1", "2-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90-99")) +
+  scale_y_continuous("# births per woman (per day)") +
+  ggtitle("Brazil age-specific birth rates") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+save_plot(p, "figures", "age_specific_birth_rates", wdt = 12, hgt = 8)
+  
+write_out_rds(br_brazil_age_2, "output", "age_specific_birth_rates")

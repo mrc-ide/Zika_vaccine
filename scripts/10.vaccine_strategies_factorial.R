aecc71cb486850d91f58582be3a07a885169e900
Lorenzo Cattarino <lcattari@dide.local>
2020-02-11 15:56:04 +0000

    save plot

--- scripts/10.vaccine_strategies_factorial.R
+++ scripts/10.vaccine_strategies_factorial.R
@@ -10,12 +10,13 @@ source(file.path("R", "wrapper_multi_factors_ZikaModel.R"))
 source(file.path("R", "calculate_MC_numbers.R"))
 source(file.path("R", "reshape.R"))
 source(file.path("R", "post_process.R"))
+source(file.path("R", "plot_diagnostics.R"))
 
 
 # define parameters -----------------------------------------------------------
 
 
-experiment_name <- "vaccine_strategies"
+experiment_name <- "vaccine_strategies_factorial"
 
 out_fig_dir <- file.path("figures", experiment_name)
 
@@ -43,31 +44,34 @@ integer_time_steps <- (364 * time_years) / my_dt
 
 its <- seq(0, integer_time_steps, 1)
 
+tt <- its * my_dt
+
+time <- max(tt)
+
 mr_baseline <- 0.0002
 
 N_human_brazil <- 200000000
-season <- FALSE
-
-plot_interval <- 5 # years
 
 vacc_child_coverage_values <- c(0.5, 0.8)
 
 population_coverage_values <- c(0.5, 1)
   
-vacc_starttime <- 1.5 * 364  
+vacc_starttime <- 1.5
 
-vacc_stoptime <- vacc_starttime + 60
+vacc_stoptime <- vacc_starttime + 0.2
 
-# from 9 to 49
-vacc_ages <- c(0,0,1,1,1,1,0,0,0,0,0) 
+vacc_ages <- c(0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0) 
+
+tot_cov_values <- c(0, as.vector(outer(vacc_child_coverage_values, population_coverage_values)))
+
+fixed_params <- list(DT = my_dt,
+                     N_human = N_human_brazil)
 
 
 # load data -------------------------------------------------------------------
 
 
-# microcephaly risk during pregnancy
 mr_pregn_data <- readRDS(file.path("output", "microcephaly_risk_probs.rds"))
-#mr_pregn_data[, "prob"] <- 0
 
 br_brazil_age <- readRDS(file.path("output", "age_specific_birth_rates.rds"))
 
@@ -75,12 +79,9 @@ br_brazil_age <- readRDS(file.path("output", "age_specific_birth_rates.rds"))
 # create factor combinations --------------------------------------------------
 
 
-fact_combs <- expand.grid(vacc_cov = vacc_child_coverage_values, 
-                          pop_cov = population_coverage_values,
+fact_combs <- expand.grid(tot_cov = tot_cov_values, 
                           start = vacc_starttime,
                           stop = vacc_stoptime)
-
-fact_combs$tot_cov <- fact_combs$vacc_cov * fact_combs$pop_cov
 
 fact_combs <- cbind(id = seq_len(nrow(fact_combs)), fact_combs)
 
@@ -97,35 +98,30 @@ vacc_experiment <- loop(fact_combs_ls,
                         agec = age_init,
                         death = deathrt,
                         vaccine_age = vacc_ages,
-                        my_dt = my_dt,
+                        params = fixed_params,
                         integer_time_steps = its,
-                        season = season,
                         parallel = FALSE)
-
-## write_out_rds(vacc_experiment, out_tab_dir, "vaccination_experiment")
 
 
 # plot number of new infections -----------------------------------------------
 
 
-tt <- its * my_dt
+sum_all_fact <- lapply(vacc_experiment, sum_across_array_dims, 1)
 
-time <- max(tt)
+sum_all_fact_id <- lapply(seq_along(sum_all_fact), cbind_id_time, sum_all_fact, tt)
 
-brks <- seq(from = 0, to = time, by = 364 * plot_interval)
+sum_all <- do.call("rbind", sum_all_fact_id)
 
-sum_over_ages <- lapply(vacc_experiment, sum_across_array_dims, c(1,3,4))
+sum_all_df <- as.data.frame(sum_all)
 
-sum_over_ages_patches <- lapply(vacc_experiment, sum_across_array_dims, c(1,3))
-
-melt_2 <- lapply(sum_over_ages, melt_sim_output_array_2, tt)
+sum_all_df$id <- factor(sum_all_df$id, 
+                        levels = unique(sum_all_df$id),
+                        labels = unique(sum_all_df$id))
   
-melt_3 <- lapply(sum_over_ages_patches, melt_sim_output_array_3, tt)  
+p <- plot_diagnostics_by_vaccine(sum_all_df, "id", "new infections")
 
-melt_3_id <- lapply(seq_along(melt_3), cbind_id, melt_3)
-  
-summary_all <- do.call("rbind", melt_3_id)
 
-p <- ggplot(data = summary_all) +
-  geom_line(aes(x = time, y = value, colour = vaccine_status)) +
-  facet_wrap(~ id)
+# save plots ------------------------------------------------------------------
+
+
+save_plot(p, out_fig_dir, "inf_1_by_run_id", wdt = 12, hgt = 8)

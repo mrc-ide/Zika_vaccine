b43f664e34bdb95e5266cc365a462fda152f3bf1
Lorenzo Cattarino <lcattari@dide.local>
2020-02-12 08:37:49 +0000

    add more functions

--- R/post_process.R
+++ R/post_process.R
@@ -1,3 +1,18 @@
+add_diagno_name_var <- function(x, b){
+  b[[x]]$diagnostics <- names(b[x])
+  b[[x]]
+}
+
+add_time_var <- function(x){
+  x$time <- tt
+  x
+}
+
+give_col_names <- function(x){
+  colnames(x) <- as.character(seq_len(11))
+  x
+}
+
 cbind_id_time <- function(i, dat, time) {
   
   # browser()

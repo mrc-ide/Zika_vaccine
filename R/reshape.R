5bce996b16d21dd8205547e5c3abc43b6952495c
Lorenzo Cattarino <lcattari@dide.local>
2020-02-19 11:41:59 +0000

    add many functions

--- R/reshape.R
+++ R/reshape.R
@@ -48,6 +48,93 @@ melt_sim_output_array_3 <- function(array_to_melt, TIME, var_name) {
   
 }
 
+aggregate_and_reshape_total <- function(my_array, tt) {
+  
+  sum_apv <- sum_across_array_dims(my_array, 1)  
+  
+  cbind_time(sum_apv, tt)
+  
+}
+
+cumsum_and_incidence_total <- function(my_array, Ntotal) {
+  
+  sum_apv <- sum_across_array_dims(my_array, 1)
+  
+  sum_apv_Ntotal <- sum_across_array_dims(Ntotal, 1)
+  
+  cumsum_sum_apv <- cumsum(sum_apv)
+  
+  window_inc <- calculate_incidence(cumsum_sum_apv, sum_apv_Ntotal, 7)
+  
+  cbind_time(window_inc, tt)
+  
+}
+
+aggregate_and_reshape_patch <- function(my_array, tt) {
+  
+  sum_av <- sum_across_array_dims(my_array, c(1, 4))
+  
+  melt_sim_output_array_3(sum_av, tt, "patch")
+  
+}
+
+cumsum_and_incidence_patch <- function(my_array, Ntotal) {
+  
+  sum_av <- sum_across_array_dims(my_array, c(1, 4))
+  
+  sum_av_Ntotal <- sum_across_array_dims(Ntotal, c(1, 4))
+  
+  cumsum_sum_av <- cumsum_across_array_dims(sum_av, 2)
+  
+  window_inc <- calculate_incidence(cumsum_sum_av, sum_av_Ntotal, 7)
+  
+  melt_sim_output_array_3(window_inc, tt, "patch")
+  
+}
+
+
+aggregate_and_reshape_vaccine <- function(my_array, tt) {
+  
+  sum_ap <- sum_across_array_dims(my_array, c(1, 3))
+  
+  melt_sim_output_array_3(sum_ap, tt, "vaccine")
+  
+}
+
+cumsum_and_incidence_vaccine <- function(my_array, Ntotal) {
+  
+  sum_ap <- sum_across_array_dims(my_array, c(1, 3))
+  
+  sum_ap_Ntotal <- sum_across_array_dims(Ntotal, c(1, 3))
+  
+  cumsum_sum_ap <- cumsum_across_array_dims(sum_ap, 2)
+  
+  window_inc <- calculate_incidence(cumsum_sum_ap, sum_ap_Ntotal, 7)
+  
+  melt_sim_output_array_3(window_inc, tt, "vaccine")
+  
+}
+
+aggregate_and_reshape_age <- function(my_array, tt) {
+  
+  melt_array <- melt_sim_output_array(my_array, tt)
+  
+  subset(melt_array, patch == 1)
+  
+}
+
+cumsum_and_incidence_age <- function(my_array, Ntotal) {
+  
+  cumsum_array <- cumsum_across_array_dims(my_array, c(2, 3, 4))
+  
+  window_inc <- calculate_incidence(cumsum_array, Ntotal, 7)
+  
+  melt_array <- melt_sim_output_array(window_inc, tt)
+  
+  subset(melt_array, patch == 1)
+  
+}
+
 # melt_sim_output_array_2 <- function(array_to_melt, TIME) {
 #   
 #   full_melt <- reshape2::melt(array_to_melt)

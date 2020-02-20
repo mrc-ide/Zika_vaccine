be6c560e43e1e21eda900745cefb042fd780118f
Lorenzo Cattarino <lcattari@dide.local>
2020-02-20 14:11:32 +0000

    add function for stripping off legend; add support for adding common legend and for set number of plots per file

--- R/plot_layout.R
+++ R/plot_layout.R
@@ -1,7 +1,15 @@
 
 # functions for manipulating layout of plots 
 
-g_legend <- function(a.gplot){
+strip_gglegend <- function(my_plot) {
+  
+  p <- my_plot + theme(legend.position = "none")
+  
+  p
+  
+}
+  
+  g_legend <- function(a.gplot){
   
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
   
@@ -13,26 +21,54 @@ g_legend <- function(a.gplot){
   
 }
 
-arrangeGrob_multi_files <- function(plots_list) {
+arrangeGrob_multi_files <- function(plots_list, n_plots_per_file, legend = FALSE) {
   
-  # arrange 4 plots per page in a 2x2 table format 
-  # return a list of grob obects
+  # arrange n plots per page in a Z x 2 table format (Z = 2 or 1)
+  # return a list of grob obects. 
   
-  n_plots_per_file <- 4
+  if(legend) {
     
+    mylegend <- g_legend(plots_list[[1]])
+    
+    plots_list <- lapply(plots_list, strip_gglegend)
+    
+  }
+
   n <- length(plots_list)
   
   n_files <- ceiling(n / n_plots_per_file)
   
   out <- vector("list", length = n_files)
   
+  if(n_plots_per_file == 2) {
+    
+    n_row <- 1 
+    
+  }
+  
+  if(n_plots_per_file == 4) {
+    
+    n_row <- 2
+    
+  }
+  
   start <- 1
   end <- n_plots_per_file
   
   for (i in seq_len(n_files)) {
     
-    out[[i]] <- arrangeGrob(grobs = plots_list[start:end], nrow = 2, ncol = 2)
+    ret <- arrangeGrob(grobs = plots_list[start:end], nrow = n_row, ncol = 2)
     
+    if(legend) {
+      
+      ret <- arrangeGrob(ret, mylegend, 
+                         nrow = 2, 
+                         widths = 17, 
+                         heights = c(11, 1))
+      
+    }
+    
+    out[[i]] <- ret 
     start <- start + n_plots_per_file
     end <- end + n_plots_per_file
     

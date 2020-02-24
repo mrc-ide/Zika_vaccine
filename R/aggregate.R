901a7df842286e4c928f39edb58b96d7879a8efb
Lorenzo Cattarino <lcattari@dide.local>
2020-02-24 16:50:38 +0000

    revert back to ifelse

--- R/aggregate.R
+++ R/aggregate.R
@@ -93,7 +93,7 @@ calculate_incidence <- function(cum_infections, Ntotal, time_window) {
     
   }
   
-  if(Ntotal == 0) 0 else out / Ntotal * 1000
+  ifelse(Ntotal == 0, 0, out / Ntotal * 1000)
   
 }
 

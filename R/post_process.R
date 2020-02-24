552cc09375fb0bc51021715245f08df5bc6e63a8
Lorenzo Cattarino <lcattari@dide.local>
2020-02-24 12:30:13 +0000

    add function for post processing vaccine strategies output

--- R/post_process.R
+++ R/post_process.R
@@ -21,20 +21,44 @@ cbind_time <- function(x, time) {
   
 }
 
-cbind_id_time <- function(i, dat, time) {
+cbind_id_time <- function(x, id, time) {
   
   # browser()
-  
-  value <- dat[[i]]
     
-  out <- cbind(id = i, time = time, value)  
+  out <- cbind(id = id, time = time, value = x)  
   
-  as.data.frame(out)
+  out_df <- as.data.frame(out)
+  
+  out_df$id <- factor(out_df$id, 
+                      levels = unique(out_df$id),
+                      labels = unique(out_df$id))
+  
+  out_df
+}
+
+cbind_id <- function(x, id) {
+  
+  cbind(x, id = id)
+
+}
+
+subset_array <- function(my_array, id_1, id_2) {
+  
+  my_array[id_1:id_2,,,]  
   
 }
 
-cbind_id <- function(x, ls) {
+vacc_strategies_post_processing <- function(x, id, time) {
   
-  cbind(ls[[x]], id = x)
-
+  out_1 <- cbind_id_time(x = x, id = id, time = time)
+  
+  out_2 <- cumsum(out_1[, "inf_1"])
+  
+  out_3 <- cumsum(out_1[, "MC"])
+  
+  out_1$inf_1_IR <- calculate_incidence(out_2, out_1$Ntotal, time_window = 7)
+  
+  out_1$MC_IR <- calculate_incidence(out_3, out_1$Ntotal, time_window = 7) 
+  
+  out_1
 }

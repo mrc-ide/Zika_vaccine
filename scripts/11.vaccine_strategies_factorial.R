ee1bf45c9a85d80fd1a1d898f23d162b44f5b55c
Lorenzo Cattarino <lcattari@dide.local>
2020-02-26 14:32:12 +0000

    add cluster support

--- scripts/11.vaccine_strategies_factorial.R
+++ scripts/11.vaccine_strategies_factorial.R
@@ -1,12 +1,53 @@
 
 ## devtools::install_github("mrc-ide/ZikaModel")
 
-library(ZikaModel)
+options(didehpc.cluster = "fi--didemrchnb")
 
-source(file.path("R", "utility_functions.R"))
-source(file.path("R", "calculate_MC_numbers.R"))
-source(file.path("R", "pre_process.R"))
-source(file.path("R", "wrapper_multi_factors_ZikaModel.R"))
+CLUSTER <- TRUE
+
+my_resources <- c(file.path("R", "utility_functions.R"),
+                  file.path("R", "calculate_MC_numbers.R"),
+                  file.path("R", "pre_process.R"),
+                  file.path("R", "wrapper_multi_factors_ZikaModel.R"))
+
+my_pkgs <- "ZikaModel"
+
+if (CLUSTER) {
+  
+  # running out of place
+  
+  workdir <- "Q:/CHIK_vaccine"
+  didehpc::didehpc_config_global(workdir = workdir)
+  root <- file.path(workdir, "context")
+  
+} else {
+  
+  root <- "context"  
+  
+}  
+
+context::context_log_start()
+ctx <- context::context_save(path = root,
+                             packages = my_pkgs,
+                             sources = my_resources,
+                             package_sources = provisionr::package_sources(github = "mrc-ide/ZikaModel"))
+
+
+# are you using the cluster? --------------------------------------------------
+
+
+if (CLUSTER) {
+  
+  config <- didehpc::didehpc_config(template = "24Core", rtools = TRUE)
+  
+  obj <- didehpc::queue_didehpc(ctx, config = config)
+  
+} else {
+  
+  #context::parallel_cluster_start(7, ctx)
+  context::context_load(ctx)
+  
+}
 
 
 # define parameters -----------------------------------------------------------
@@ -34,34 +75,35 @@ mr_baseline <- 0.0002
 
 plot_interval <- 5 # years
 
-vacc_coverage_values <- c(0, 0.5, 0.8, 1)
-
 vacc_starttime <- 1.7  
 
 diagnostics_to_save <- c("inf_1", "Ntotal", "MC")
 
+fixed_params <- list(vacc_child_starttime = vacc_starttime)
+
+vacc_coverage_values <- c(0, 0.5, 0.8, 1)
+
 target_pop_values <- c(1, 2)
 
-# ~2 months campaign
 duration_values <- c(0.16, time_years + 1)
 
-fixed_params <- list(vacc_child_starttime = vacc_starttime)
+eff_values <- c(0.75, 1)
 
 
 # pre processing --------------------------------------------------------------
 
 
-experiment_name <- file.path("vaccine_strategies", "factorial")
+experiment_name <- file.path("vaccine_strategies", "factorial_2")
 
 out_tab_dir <- file.path("output", experiment_name)
 
 exp_des <- expand.grid(vacc_cov = vacc_coverage_values,
                        target_pop = target_pop_values,
                        duration = duration_values, 
+                       efficacy = eff_values,
                        stringsAsFactors = FALSE)
 
-exp_des <- cbind(id = sprintf("%02d", seq_len(nrow(exp_des))),
-                 exp_des)
+exp_des$id <- sprintf("%02d", seq_len(nrow(exp_des)))
 
 integer_time_steps <- (364 * time_years) / my_dt
 
@@ -78,21 +120,57 @@ mr_pregn_data <- readRDS(file.path("output", "microcephaly_risk_probs.rds"))
 br_brazil_age <- readRDS(file.path("output", "age_specific_birth_rates.rds"))
 
 
+# run one job -----------------------------------------------------------------
+
+
+# t <- obj$enqueue(wrapper_multi_factors_ZikaModel_2(
+#   fact_combs_ls[[1]],
+#   agec = age_init,
+#   death = deathrt,
+#   parms = fixed_params,
+#   integer_time_steps = its,
+#   var_save = diagnostics_to_save,
+#   out_dir = out_tab_dir,
+#   mr_pregn_risk = mr_pregn_data,
+#   birth_rates = br_brazil_age,
+#   mr_baseline = mr_baseline))
+
+
 # run -------------------------------------------------------------------------
 
 
-vaccine_exps <- loop(fact_combs_ls,
-                     wrapper_multi_factors_ZikaModel_2,
-                     agec = age_init,
-                     death = deathrt,
-                     parms = fixed_params,
-                     integer_time_steps = its,
-                     var_save = diagnostics_to_save,
-                     out_dir = out_tab_dir,
-                     mr_pregn_risk = mr_pregn_data,
-                     birth_rates = br_brazil_age,
-                     mr_baseline = mr_baseline,
-                     parallel = FALSE)
+if (CLUSTER) {
+  
+  vaccine_exps <- queuer::qlapply(
+    fact_combs_ls,
+    wrapper_multi_factors_ZikaModel_2,
+    obj,
+    agec = age_init,
+    death = deathrt,
+    parms = fixed_params,
+    integer_time_steps = its,
+    var_save = diagnostics_to_save,
+    out_dir = out_tab_dir,
+    mr_pregn_risk = mr_pregn_data,
+    birth_rates = br_brazil_age,
+    mr_baseline = mr_baseline)
+  
+} else {
+  
+  vaccine_exps <- loop(fact_combs_ls,
+                       wrapper_multi_factors_ZikaModel_2,
+                       agec = age_init,
+                       death = deathrt,
+                       parms = fixed_params,
+                       integer_time_steps = its,
+                       var_save = diagnostics_to_save,
+                       out_dir = out_tab_dir,
+                       mr_pregn_risk = mr_pregn_data,
+                       birth_rates = br_brazil_age,
+                       mr_baseline = mr_baseline,
+                       parallel = FALSE)
+  
+}
 
 
 # save output -----------------------------------------------------------------

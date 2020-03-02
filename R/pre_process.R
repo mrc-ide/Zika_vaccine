881558dcbeb0aa06424e457093080d4c325c9bdb
Lorenzo Cattarino <lcattari@dide.local>
2020-03-02 12:28:48 +0000

    change vaccination start data (for different pre existing immunity levels)

--- R/pre_process.R
+++ R/pre_process.R
@@ -24,17 +24,17 @@ get_vacc_start_time <- function(preexisting_immunity_level) {
     
     if(preexisting_immunity_level == 0) {
       
-      out <- 1.7
+      out <- 1.3
     }  
     
     if(preexisting_immunity_level == 0.25) {
       
-      out <- 2.2
+      out <- 1.75
     }
     
     if(preexisting_immunity_level == 0.5) {
       
-      out <- 3.5
+      out <- 2.5
       
     }
     

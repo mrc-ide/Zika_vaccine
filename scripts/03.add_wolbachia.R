4c1b6c3b927ae0d5f4ae1581ada4c155df701bcc
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-24 14:36:08 +0100

    update after pkg upgrade

--- scripts/03.add_wolbachia.R
+++ scripts/03.add_wolbachia.R
@@ -1,207 +1,359 @@
 
-devtools::install_github("mrc-ide/ZikaModel")
+# devtools::install_github("mrc-ide/ZikaModel")
 
 library(ZikaModel)
-library(reshape2)
 library(ggplot2)
+library(patchwork)
+library(dplyr)
 
-source(file.path("R", "save_plot.R"))
-source(file.path("R", "wrapper_to_save_plot.R"))
-source(file.path("R", "reshape.R"))
+source(file.path("R", "utility_functions.R"))
 
 
 # define parameters -----------------------------------------------------------
 
 
-out_dir <- file.path("figures", "deterministic_wol_3")
+out_dir <- file.path("figures", "wolbachia")
+out_dir_2 <- file.path(out_dir, "patch")
 
-agec <- c(1, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+hgt <- 8
+wdt <- 13
+hgt_p <- 15
+wdt_p <- 18
 
-death <- c(1e-10,
-           1e-10,
-           1e-10,
-           0.00277068683332695,
-           0.0210680857689784,
-           0.026724997685722,
-           0.0525354529367476,
-           0.0668013582441452,
-           0.119271483740379,
-           0.279105747097929,
-           0.390197266957464)
-
-time_years <- 50 # years
-
-my_dt <- 1
-
-odin_model_path <- system.file("extdata/odin_model_determ.R", package = "ZikaModel")
-
-Wb_starttime <- 1 # years
-Wb_introduration_days <- 30 # days
-Wb_introlevel <- 0.3 # proportion of the initial number of wt mosquitoes 
-
-Wb_introduration <- Wb_introduration_days / my_dt
+Wb_introlevel <- 0.1 # proportion of the initial number of wt mosquitoes 
 
 
 # run -------------------------------------------------------------------------
 
 
-create_generator <- create_r_model(odin_model_path = odin_model_path,
-                                   agec = agec,
-                                   death = death,
-                                   nn_links = nn_links,
-                                   amplitudes_phases = amplitudes_phases,
-                                   DT = my_dt,
-                                   season = TRUE,
-                                   Wb_starttime = Wb_starttime,
-                                   Wb_introduration = Wb_introduration,
-                                   Wb_introlevel = Wb_introlevel)
-
-gen <- create_generator$generator(user = create_generator$state)
-
-integer_time_steps <- (364 * time_years) / my_dt
-
-its <- seq(0, integer_time_steps, 1)
-
-mod_run <- gen$run(its)
+r1 <- run_deterministic_model(time_period = 365 * 5,
+                              Wb_introlevel = Wb_introlevel)
 
 
-# post processing -------------------------------------------------------------
+# plot totals -----------------------------------------------------------------
 
 
-out <- gen$transform_variables(mod_run)
+## format
+Ntotal <- format_output_H(r1, var_select = "Ntotal")
+inf_1 <- format_output_H(r1, var_select = "inf_1")
+MC <- format_output_H(r1, var_select = "MC")
+inf_1_w <- format_output_H(r1, var_select = "inf_1_w")
+MC_w <- format_output_H(r1, var_select = "MC_w")
 
-out_2 <- post_processing(out, my_dt)
+Lwt <- format_output_M(r1, var_select = "Lwt")
+Mwt_S <- format_output_M(r1, var_select = "Mwt_S")
+Mwt_E1 <- format_output_M(r1, var_select = "Mwt_E1")
+Mwt_E2 <- format_output_M(r1, var_select = "Mwt_E2")
+Mwt_I1 <- format_output_M(r1, var_select = "Mwt_I1")
 
-p1 <- plot_compartments(out_2$compartments)
+Lwb <- format_output_M(r1, var_select = "Lwb")
+Mwb_S <- format_output_M(r1, var_select = "Mwb_S")
+Mwb_E1 <- format_output_M(r1, var_select = "Mwb_E1")
+Mwb_E2 <- format_output_M(r1, var_select = "Mwb_E2")
+Mwb_I1 <- format_output_M(r1, var_select = "Mwb_I1")
+prop_wb <- format_output_Mprop(r1)
 
-save_plot(plot_obj = p1,
-          out_pth = out_dir,
-          out_fl_nm = "human_compartments.png",
-          wdt = 14,
-          hgt = 9)
+# plot
+humans_plot <- plot(r1, type = "H")
 
-p2 <- plot_demographics(out_2$demographics)
+Ntotal_plot <- ggplot(Ntotal, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Total number of humans") +
+  theme_bw()
 
-lapply(seq_along(p2),
-       wrapper_to_save_plot,
-       p2,
-       out_fl_nm = "human_demographics",
-       out_pth = out_dir,
-       wdt = 18,
-       hgt = 10)
+inf_1_plot <- ggplot(inf_1, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Daily infections") +
+  theme_bw()
+
+MC_plot <- ggplot(MC, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Daily microcepahly cases") +
+  theme_bw()
+
+inf_1_w_plot <- ggplot(inf_1_w, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Weekly infections/1000") +
+  theme_bw()
+
+MC_w_plot <- ggplot(MC_w, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Weekly microcepahly cases/1000") +
+  theme_bw()
+
+extra_H_plot <- Ntotal_plot + inf_1_plot + MC_plot + inf_1_w_plot + MC_w_plot +
+  plot_layout(ncol = 2)
+
+Lwt_plot <- ggplot(Lwt, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Larvae wild type") +
+  theme_bw()
+
+Mwt_S_plot <- ggplot(Mwt_S, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Susceptibles wild type") +
+  theme_bw()
+
+Mwt_E1_plot <- ggplot(Mwt_E1, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 1 wild type") +
+  theme_bw()
+
+Mwt_E2_plot <- ggplot(Mwt_E2, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 2 wild type") +
+  theme_bw()
+
+Mwt_I1_plot <- ggplot(Mwt_I1, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Infectious wild type") +
+  theme_bw()
+
+extra_Mwt_plot <- Lwt_plot + Mwt_S_plot + Mwt_E1_plot +  Mwt_E2_plot +
+  Mwt_I1_plot + plot_layout(ncol = 2)
+
+Lwb_plot <- ggplot(Lwb, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Larvae wb type") +
+  theme_bw()
+
+Mwb_S_plot <- ggplot(Mwb_S, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Susceptibles wb type") +
+  theme_bw()
+
+Mwb_E1_plot <- ggplot(Mwb_E1, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 1 wb type") +
+  theme_bw()
+
+Mwb_E2_plot <- ggplot(Mwb_E2, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 2 wb type") +
+  theme_bw()
+
+Mwb_I1_plot <- ggplot(Mwb_I1, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Infectious wb type") +
+  theme_bw()
+
+prop_wb_plot <- ggplot(prop_wb, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("Proportion") +
+  ggtitle("Proportion of wb type") +
+  theme_bw()
+
+extra_Mwb_plot <- Lwb_plot + Mwb_S_plot + Mwb_E1_plot +  Mwb_E2_plot +
+  Mwb_I1_plot + prop_wb_plot + plot_layout(ncol = 2)
 
 
-# extract mosquitoes diagnostics ----------------------------------------------
+# save
+save_plot(humans_plot, out_dir, "human_compartments", wdt = wdt, hgt = hgt)
+save_plot(extra_H_plot, out_dir, "extra_H_diagnostics", wdt = 18, hgt = 15)
+save_plot(extra_Mwt_plot, out_dir, "extra_Mwt_diagnostics", wdt = 18, hgt = 15)
+save_plot(extra_Mwb_plot, out_dir, "extra_Mwb_diagnostics", wdt = 18, hgt = 15)
 
 
-diagno_mos_wt <- c("Lwt", "Mwt_S", "Mwt_E1", "Mwt_E2", "Mwt_I1", "Mwt_tot", "Lwt_birth",
-                   "Lwt_mature", "Mwt_inf1")
-
-diagno_mos_wb <- c("Lwb", "Mwb_S", "Mwb_E1", "Mwb_E2", "Mwb_I1", "Mwb_tot", "Lwb_birth",
-                   "Lwb_mature", "Mwb_inf1", "Mwb_intro")
-
-diagno_mos <- c(diagno_mos_wt, diagno_mos_wb)
-
-dia_mos <- setNames(out[diagno_mos], diagno_mos)
-
-mossum <- lapply(dia_mos, function(x){apply(x, 1, sum)})
-
-mat_M <- do.call("cbind", mossum)
-
-df_M <- as.data.frame(mat_M)
-
-tt <- out$TIME
-time <- max(tt)
-
-df_M$time <- tt
-df_M_melt <- melt(df_M,
-                  id.vars = "time",
-                  variable.name = "diagnostic")
-
-diagno_levs <- c(diagno_mos_wt, diagno_mos_wb)
-
-df_M_melt$diagnostic <- factor(df_M_melt$diagnostic, levels = diagno_levs, labels = diagno_levs)
-
-p3 <- plot_demographics(df_M_melt)
-
-lapply(seq_along(p3),
-       wrapper_to_save_plot,
-       p3,
-       out_fl_nm = "mosquitoes_demographics",
-       out_pth = out_dir,
-       wdt = 18,
-       hgt = 10)
+# plot by patch ---------------------------------------------------------------
 
 
+## format
+Ntotal_p <- format_output_H(r1, var_select = "Ntotal", keep = "patch")
+inf_1_p <- format_output_H(r1, var_select = "inf_1", keep = "patch")
+MC_p <- format_output_H(r1, var_select = "MC", keep = "patch")
+inf_1_w_p <- format_output_H(r1, var_select = "inf_1_w", keep = "patch")
+MC_w_p <- format_output_H(r1, var_select = "MC_w", keep = "patch")
 
-# -----------------------------------------------------------------------------
-#
-# Plot mosquitoes diagnostics by patch
-#
-# -----------------------------------------------------------------------------
+Lwt_p <- format_output_M(r1, var_select = "Lwt", keep = "patch")
+Mwt_S_p <- format_output_M(r1, var_select = "Mwt_S", keep = "patch")
+Mwt_E1_p <- format_output_M(r1, var_select = "Mwt_E1", keep = "patch")
+Mwt_E2_p <- format_output_M(r1, var_select = "Mwt_E2", keep = "patch")
+Mwt_I1_p <- format_output_M(r1, var_select = "Mwt_I1", keep = "patch")
 
+Lwb_p <- format_output_M(r1, var_select = "Lwb", keep = "patch")
+Mwb_S_p <- format_output_M(r1, var_select = "Mwb_S", keep = "patch")
+Mwb_E1_p <- format_output_M(r1, var_select = "Mwb_E1", keep = "patch")
+Mwb_E2_p <- format_output_M(r1, var_select = "Mwb_E2", keep = "patch")
+Mwb_I1_p <- format_output_M(r1, var_select = "Mwb_I1", keep = "patch")
+prop_wb_p <- format_output_Mprop(r1, keep = "patch")
 
+# plot
+humans_p_plot <- plot(r1, type = "H", keep = "patch")
 
-out_dir_1 <- file.path(out_dir, "patch")
+Ntotal_p_plot <- ggplot(Ntotal_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Total number of humans") +
+  theme_bw()
 
-my_breaks_2 <- seq(from = 0, to = time, by = 364 * 10)
+inf_1_p_plot <- ggplot(inf_1_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Daily infections") +
+  theme_bw()
 
-diagno_mos_reshaped <- lapply(diagno_mos, reshape_by_patch, out)
+MC_p_plot <- ggplot(MC_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Daily microcepahly cases") +
+  theme_bw()
 
-for (i in seq_along(diagno_mos_reshaped)) {
-  
-  one_dat <- diagno_mos_reshaped[[i]]
-  
-  one_ggttl <- diagno_mos[i]
-  
-  one_plot <- ggplot(data = one_dat, aes(x = x, y = y)) +
-    geom_line(color = 'royalblue', size = 0.5) +
-    facet_wrap(~ patch) +
-    scale_x_continuous("Years", breaks = my_breaks_2, labels = my_breaks_2/364) +
-    scale_y_continuous("") +
-    ggtitle(one_ggttl) +
-    theme_bw(base_size = 10)
-  
-  out_fl_nm <- paste0(one_ggttl, ".png")
-  
-  save_plot(plot_obj = one_plot, out_pth = out_dir_1, 
-            out_fl_nm = out_fl_nm, wdt = 18, hgt = 15)
-  
-}
+inf_1_w_p_plot <- ggplot(inf_1_w_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Weekly infections/1000") +
+  theme_bw()
 
+MC_w_p_plot <- ggplot(MC_w_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Weekly microcepahly cases/1000") +
+  theme_bw()
 
-# plot proportions ------------------------------------------------------------
+Lwt_p_plot <- ggplot(Lwt_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Larvae wild type") +
+  theme_bw()
 
-# it does not make sense to take the mean across patches of these 
-# if seasonality is ON
+Mwt_S_p_plot <- ggplot(Mwt_S_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Susceptibles wild type") +
+  theme_bw()
 
+Mwt_E1_p_plot <- ggplot(Mwt_E1_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 1 wild type") +
+  theme_bw()
 
-diagno_mos_prop <- c("Mwt_propinf", "Mwb_propinf", "M_propinf", "prop_wb")
+Mwt_E2_p_plot <- ggplot(Mwt_E2_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 2 wild type") +
+  theme_bw()
 
-diagno_mos_reshaped_2 <- lapply(diagno_mos_prop, reshape_by_patch, out)
+Mwt_I1_p_plot <- ggplot(Mwt_I1_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Infectious wild type") +
+  theme_bw()
 
-y_brks <- seq(0, 1, 0.2)
+Lwb_p_plot <- ggplot(Lwb_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Larvae wb type") +
+  theme_bw()
 
-for (i in seq_along(diagno_mos_reshaped_2)) {
-  
-  one_dat <- diagno_mos_reshaped_2[[i]]
-  
-  one_ggttl <- diagno_mos_prop[i]
-    
-  one_plot <- ggplot(data = one_dat, aes(x = x, y = y)) +
-    geom_line(color = 'royalblue', size = 0.5) +
-    facet_wrap(~ patch) +
-    scale_x_continuous("Years", breaks = my_breaks_2, labels = my_breaks_2/364) +
-    scale_y_continuous("Proportion", 
-                       breaks = y_brks, 
-                       labels = y_brks, 
-                       limits = c(min(y_brks), max(y_brks))) +
-    ggtitle(one_ggttl) +
-    theme_bw(base_size = 10)
-  
-  out_fl_nm <- paste0(one_ggttl, ".png")
-  
-  save_plot(plot_obj = one_plot, out_pth = out_dir_1, 
-            out_fl_nm = out_fl_nm, wdt = 18, hgt = 15)
-  
-}
+Mwb_S_p_plot <- ggplot(Mwb_S_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Susceptibles wb type") +
+  theme_bw()
+
+Mwb_E1_p_plot <- ggplot(Mwb_E1_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 1 wb type") +
+  theme_bw()
+
+Mwb_E2_p_plot <- ggplot(Mwb_E2_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Exposed stage 2 wb type") +
+  theme_bw()
+
+Mwb_I1_p_plot <- ggplot(Mwb_I1_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("N") +
+  ggtitle("Infectious wb type") +
+  theme_bw()
+
+prop_wb_p_plot <- ggplot(prop_wb_p, aes(x = t, y = y)) +
+  geom_line(color = 'royalblue', size = 0.5) +
+  facet_wrap(~ patch) +
+  scale_x_continuous("Time") +
+  scale_y_continuous("Proportion") +
+  ggtitle("Proportion of wb type mosquitoes") +
+  theme_bw()
+
+# save
+save_plot(humans_p_plot, out_dir_2, "human_compartments", wdt = wdt_p, hgt = hgt_p)
+save_plot(Ntotal_p_plot, out_dir_2, "Ntotal", wdt = wdt_p, hgt = hgt_p)
+save_plot(inf_1_p_plot, out_dir_2, "inf_1", wdt = wdt_p, hgt = hgt_p)
+save_plot(MC_p_plot, out_dir_2, "MC", wdt = wdt_p, hgt = hgt_p)
+save_plot(inf_1_w_p_plot, out_dir_2, "inf_1_w", wdt = wdt_p, hgt = hgt_p)
+save_plot(MC_w_p_plot, out_dir_2, "MC_w", wdt = wdt_p, hgt = hgt_p)
+save_plot(Lwt_p_plot, out_dir_2, "Lwt", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwt_S_p_plot, out_dir_2, "Mwt_S", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwt_E1_p_plot, out_dir_2, "Mwt_E1", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwt_E2_p_plot, out_dir_2, "Mwt_E2", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwt_I1_p_plot, out_dir_2, "Mwt_I1", wdt = wdt_p, hgt = hgt_p)
+save_plot(Lwb_p_plot, out_dir_2, "Lwb", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwb_S_p_plot, out_dir_2, "Mwb_S", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwb_E1_p_plot, out_dir_2, "Mwb_E1", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwb_E2_p_plot, out_dir_2, "Mwb_E2", wdt = wdt_p, hgt = hgt_p)
+save_plot(Mwb_I1_p_plot, out_dir_2, "Mwb_I1", wdt = wdt_p, hgt = hgt_p)
+save_plot(prop_wb_p_plot, out_dir_2, "prop_wb", wdt = wdt_p, hgt = hgt_p)

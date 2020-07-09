8e037b8dfe489dffda4801d3b6982e692c2bb9da
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-07-09 16:11:55 +0100

    delete function

--- R/post_process.R
+++ R/post_process.R
@@ -42,22 +42,3 @@ subset_array <- function(my_array, id_1, id_2) {
   my_array[id_1:id_2,,,]  
   
 }
-
-vacc_strategies_post_processing <- function(x, id, time) {
-  
-  out_1 <- cbind_id_time(x = x[, "inf_1"], id = id, time = time)
-  
-  names(out_1)[names(out_1) == "value"] <- "inf_1"
-  
-  out_2 <- cbind(out_1, MC = x[, "MC"], Ntotal = x[, "Ntotal"])
-  
-  out_3 <- cumsum(out_2[, "inf_1"])
-  
-  out_4 <- cumsum(out_2[, "MC"])
-  
-  out_2$inf_1_IR <- calculate_incidence(out_3, out_2$Ntotal, time_window = 7)
-  
-  out_2$MC_IR <- calculate_incidence(out_4, out_2$Ntotal, time_window = 7) 
-  
-  out_2
-}

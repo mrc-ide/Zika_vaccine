c4dab3487183d0bd4b636fb40465f72ca5a497fa
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-07-09 20:34:02 +0100

    update script after pkg update

--- scripts/07.vaccine_strategies_factorial.R
+++ scripts/07.vaccine_strategies_factorial.R
@@ -5,10 +5,8 @@ options(didehpc.cluster = "fi--didemrchnb")
 
 CLUSTER <- FALSE
 
-my_resources <- c(file.path("R", "pre_process.R"),
-                  file.path("R", "utility_functions.R"),
-                  file.path("R", "calculate_MC_numbers.R"),
-                  file.path("R", "wrapper_multi_factors_ZikaModel.R"))
+my_resources <- c(file.path("R", "utility_functions.R"),
+                  file.path("R", "vaccine.R"))
 
 my_pkgs <- "ZikaModel"
 
@@ -44,7 +42,7 @@ if (CLUSTER) {
   
 } else {
   
-  #context::parallel_cluster_start(7, ctx)
+  context::parallel_cluster_start(5, ctx)
   context::context_load(ctx)
   
 }
@@ -55,37 +53,10 @@ if (CLUSTER) {
 
 experiment_no <- 1
 
-experiment_name <- paste0("factorial_", experiment_no)
-  
-experiment_path <- file.path("vaccine_strategies", experiment_name)
+vacc_starttime <- 1.3  
 
-age_init <- c(1, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-
-deathrt <- c(1e-10,
-             1e-10,
-             1e-10,
-             0.00277068683332695,
-             0.0210680857689784,
-             0.026724997685722,
-             0.0525354529367476,
-             0.0668013582441452,
-             0.119271483740379,
-             0.279105747097929,
-             0.390197266957464)
-
-time_years <- 50 # years
-
-my_dt <- 1
-
-mr_baseline <- 0.0002
-
-plot_interval <- 5 # years
-
-vacc_starttime <- 1.7  
-
-fixed_params <- list(vacc_cu_time = vacc_starttime)
-
-diagnostics_to_save <- c("inf_1", "Ntotal", "MC")
+fixed_params <- list(vacc_starttime = vacc_starttime,
+                     time_period = 6)
 
 vacc_coverage_values <- c(0, 0.5, 0.8, 1)
 
@@ -93,54 +64,36 @@ target_pop_values <- c(1, 2)
 
 duration_values <- c(0.17, 1.5, 2)
 
-eff_values <- 0.75
-
 prop_immune_values <- c(0, 0.25, 0.5)
 
 
 # pre processing --------------------------------------------------------------
 
 
+experiment_name <- paste0("factorial_", experiment_no)
+  
+experiment_path <- file.path("vaccine_strategies", experiment_name)
+
 out_tab_dir <- file.path("output", experiment_path)
 
 exp_des <- expand.grid(vacc_cov = vacc_coverage_values,
                        target_pop = target_pop_values,
                        duration = duration_values, 
-                       efficacy = eff_values,
                        prop_imm = prop_immune_values,
                        stringsAsFactors = FALSE)
 
 exp_des$id <- sprintf("%02d", seq_len(nrow(exp_des)))
 
-integer_time_steps <- (364 * time_years) / my_dt
-
-its <- seq(0, integer_time_steps, 1)
-
 fact_combs_ls <- df_to_list(exp_des, use_names = TRUE)
   
-
-# load data -------------------------------------------------------------------
-
-
-mr_pregn_data <- readRDS(file.path("output", "microcephaly_risk_probs.rds"))
-
-br_brazil_age <- readRDS(file.path("output", "age_specific_birth_rates.rds"))
-
 
 # run one job -----------------------------------------------------------------
 
 
-# t <- obj$enqueue(wrapper_multi_factors_ZikaModel_2(
+# t <- obj$enqueue(wrapper_vaccine_factorial(
 #   fact_combs_ls[[1]],
-#   agec = age_init,
-#   death = deathrt,
 #   parms = fixed_params,
-#   integer_time_steps = its,
-#   var_save = diagnostics_to_save,
-#   out_dir = out_tab_dir,
-#   mr_pregn_risk = mr_pregn_data,
-#   birth_rates = br_brazil_age,
-#   mr_baseline = mr_baseline))
+#   out_dir = out_tab_dir))
 
 
 # run -------------------------------------------------------------------------
@@ -150,38 +103,25 @@ if (CLUSTER) {
   
   vaccine_exps <- queuer::qlapply(
     fact_combs_ls,
-    wrapper_multi_factors_ZikaModel_2,
+    wrapper_vaccine_factorial,
     obj,
-    agec = age_init,
-    death = deathrt,
     parms = fixed_params,
-    integer_time_steps = its,
-    var_save = diagnostics_to_save,
     out_dir = out_tab_dir,
-    mr_pregn_risk = mr_pregn_data,
-    birth_rates = br_brazil_age,
-    mr_baseline = mr_baseline,
     name = experiment_name)
   
 } else {
   
   vaccine_exps <- loop(fact_combs_ls,
-                       wrapper_multi_factors_ZikaModel_2,
-                       agec = age_init,
-                       death = deathrt,
+                       wrapper_vaccine_factorial,
                        parms = fixed_params,
-                       integer_time_steps = its,
-                       var_save = diagnostics_to_save,
                        out_dir = out_tab_dir,
-                       mr_pregn_risk = mr_pregn_data,
-                       birth_rates = br_brazil_age,
-                       mr_baseline = mr_baseline,
-                       parallel = FALSE)
+                       parallel = TRUE)
   
 }
 
 
-# save output -----------------------------------------------------------------
+# save ------------------------------------------------------------------------
 
 
-write_out_csv(exp_des, out_tab_dir, paste0("experimental_design_", experiment_no)) 
+write_out_csv(exp_des, out_tab_dir, paste0("experimental_design_", experiment_no), 
+              row.names = FALSE) 
